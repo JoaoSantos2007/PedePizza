6624e0e0dfaf11e6479d43daca1d2b590ecfd950
JoaoSantos2007 <joao.santos.2007sp@gmail.com>
2023-07-14 23:11:17 -0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from './src/app.js';
-import { PORT } from './src/utils/env.js'
+import { PORT } from './src/utils/env.js';
 
 app.listen(PORT, () => {
   console.log(`Server is working on http://localhost:${PORT}`);
