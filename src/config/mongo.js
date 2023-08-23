bca046b68f8f1a2c5f400f8f62f2aed18baf00ff
JoaoSantos2007 <joao.santos.2007sp@gmail.com>
2023-08-22 22:42:44 -0300

    update

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
-import { MONGO_URL } from '../utils/env.js';
+import { DATABASE_NAME, MONGO_URL } from '../utils/env.js';
 
 mongoose.set('strictQuery', true);
 mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
+    // eslint-disable-next-line no-console
     console.log('Database connection estabilished with success!');
   })
   .catch((err) => {
     throw new Error(`Error in database connection: ${err}`);
   });
 
-const db = mongoose.connection.useDb('pedePizza');
+const db = mongoose.connection.useDb(DATABASE_NAME);
 
 export default db;
--- src/utils/env.js
+++ src/utils/env.js
@@ -7,14 +7,15 @@ const PORT = process.env.PORT || 3000;
 const ACCESSTOKEN_LIFETIME = process.env.ACCESSTOKEN_LIFETIME || 15;
 const REFRESHTOKEN_LIFETIME = process.env.REFRESHTOKEN_LIFETIME || 52;
 const UPLOAD_PATH = process.env.UPLOAD_PATH || path.resolve('./uploads');
+const { DATABASE_NAME } = process.env;
 const { MONGO_URL } = process.env;
 const { SALT } = process.env;
 const { SECRET } = process.env;
 
-if (!MONGO_URL || !SALT || !SECRET) {
+if (!MONGO_URL || !SALT || !SECRET || !DATABASE_NAME) {
   throw new Error('It is missing the Environment Variables in the .env file at the root of the project!');
 }
 
 export {
-  PORT, REFRESHTOKEN_LIFETIME, ACCESSTOKEN_LIFETIME, UPLOAD_PATH, MONGO_URL, SALT, SECRET,
+  PORT, REFRESHTOKEN_LIFETIME, ACCESSTOKEN_LIFETIME, UPLOAD_PATH, MONGO_URL, SALT, SECRET, DATABASE_NAME,
 };
